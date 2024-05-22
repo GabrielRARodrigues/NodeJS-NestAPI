478f082f925ec096a278f4bcb74379c6609815b3
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-05-22 20:06:03 -0300

    raw: Creating the 'env.ts' file to validate the project's environment variables and share them with the rest of the application, updating the 'app.module.ts' file by importing the 'ConfigModule' to make the environment variables available to all project modules, and modifying the 'main.ts' file to obtain and use the environment variables to launch the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+
+import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controllers/create-account.controller'
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService, PrismaService]
+  imports: [ConfigModule.forRoot({
+    validate: (env) => envSchema.parse(env),
+    isGlobal: true
+  })],
+  providers: [PrismaService],
+  controllers: [CreateAccountController]
 })
 export class AppModule {}
