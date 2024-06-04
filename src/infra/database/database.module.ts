acee3b6ab040e0ed70ee53a13591bde6d5043303
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-06-04 15:37:36 -0300

    raw: Implementing the base repository 'StudentsRepository' in the infrastructure layer, creating the 'mapper' class 'PrismaStudentMapper', and creating the repository 'PrismaStudentsRepository' that implements 'StudentsRepository'. Finally, importing 'StudentsRepository' and 'PrismaStudentsRepository' in the 'DatabaseModule'.

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
 
-import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
-
 import { PrismaService } from './prisma/prisma.service'
+
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+
 import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
 import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
 import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
 import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answers-comments-repository'
@@ -17,6 +20,11 @@ import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-
       provide: QuestionsRepository,
       useClass: PrismaQuestionsRepository
     },
+
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository
+    },
     PrismaAnswersRepository,
     PrismaQuestionCommentsRepository,
     PrismaAnswerCommentsRepository,
@@ -26,6 +34,7 @@ import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-
   exports: [
     PrismaService,
     QuestionsRepository,
+    StudentsRepository,
     PrismaAnswersRepository,
     PrismaQuestionCommentsRepository,
     PrismaAnswerCommentsRepository,
