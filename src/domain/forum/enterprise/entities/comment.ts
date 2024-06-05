209a6424ddd806c781deafb4be046683e309c9a0
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-06-05 16:37:18 -0300

    raw: Creating 'mapper' classes for the remaining entities of the application. Creating 'mappers' 'PrismaAnswerMapper', 'PrismaAnswerCommentMapper', 'PrismaQuestionCommentMapper', 'PrismaAnswerAttachmentMapper', and 'PrismaQuestionAttachmentMapper'

--- src/domain/forum/enterprise/entities/comment.ts
+++ src/domain/forum/enterprise/entities/comment.ts
@@ -5,10 +5,12 @@ export interface CommentProps {
   authorId: UniqueEntityId
   content: string
   createdAt: Date
-  updatedAt?: Date
+  updatedAt?: Date | null
 }
 
-export abstract class Comment<Props extends CommentProps> extends AggregateRoot<Props> {
+export abstract class Comment<
+  Props extends CommentProps
+> extends AggregateRoot<Props> {
   get authorId() {
     return this.props.authorId
   }
