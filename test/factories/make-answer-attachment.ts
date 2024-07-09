2357a2d35f53529119fc4b607eec1a387a50ac20
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-07-09 12:04:40 -0300

    refactor: Updating 'make-answer-attachment' factory by adding the 'AnswerAttachmentFactory' class, simulating the creation of an 'AnswerAttachment' at the persistence level

--- test/factories/make-answer-attachment.ts
+++ test/factories/make-answer-attachment.ts
@@ -1,8 +1,11 @@
+import { Injectable } from '@nestjs/common'
+
 import {
   AnswerAttachmentProps,
   AnswerAttachment
 } from '@/domain/forum/enterprise/entities/answer-attachment'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 export function makeAnswerAttachment(
   override: Partial<AnswerAttachmentProps> = {},
@@ -18,4 +21,26 @@ export function makeAnswerAttachment(
   )
 
   return answerAttachment
+}
+
+@Injectable()
+export class AnswerAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswerAttachment(
+    data: Partial<AnswerAttachmentProps> = {}
+  ): Promise<AnswerAttachment> {
+    const AnswerAttachment = makeAnswerAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: AnswerAttachment.attachmentId.toString()
+      },
+      data: {
+        answerId: AnswerAttachment.answerId.toString()
+      }
+    })
+
+    return AnswerAttachment
+  }
 }
