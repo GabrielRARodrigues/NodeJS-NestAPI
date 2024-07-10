cb5526fa4d611e6cc6d298604c1523cc525f9e1f
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-07-10 20:49:42 -0300

    raw: Implementing the method 'findManyByQuestionIdWithAuthor' of the repository 'QuestionCommentsRepository' in the class 'PrismaQuestionCommentsRepository' in the infrastructure layer using the 'mapper' 'PrismaCommentWithAuthorMapper'. Updating the 'controller' for listing comments of a question ('FetchQuestionCommentsController') reflecting the changes made in the use case for listing comments of a question ('FetchQuestionCommentsUseCase') and using the presenter 'CommentWithAuthorPresenter' instead of 'CommentPresenter' and updating the controller tests reflecting the changes

--- src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
@@ -5,6 +5,8 @@ import { PrismaService } from '../prisma.service'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 import { PrismaQuestionCommentMapper } from '../mappers/prisma-question-comment-mapper'
 import { PaginationParams } from '@/core/repositories/pagination-params'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
+import { PrismaCommentWithAuthorMapper } from '../mappers/prisma-comment-with-author-mapper'
 
 @Injectable()
 export class PrismaQuestionCommentsRepository
@@ -39,6 +41,24 @@ export class PrismaQuestionCommentsRepository
     })
 
     return questionComments.map(PrismaQuestionCommentMapper.toDomain)
+  }
+
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams
+  ): Promise<CommentWithAuthor[]> {
+    const questionComments = await this.prisma.comment.findMany({
+      where: {
+        questionId
+      },
+      include: {
+        author: true
+      },
+      take: 20,
+      skip: (page - 1) * 20
+    })
+
+    return questionComments.map(PrismaCommentWithAuthorMapper.toDomain)
   }
 
   async create(questionComment: QuestionComment): Promise<void> {
