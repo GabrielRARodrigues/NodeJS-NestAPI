0f1af23690625d6029f99f16f85fc9eb02cda012
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-07-10 21:02:38 -0300

    raw: Implementing the 'findManyByAnswerIdWithAuthor' method in the 'AnswerCommentsRepository' repository within the 'PrismaAnswerCommentsRepository' class in the infrastructure layer using the 'PrismaCommentWithAuthorMapper' mapper. Updating the 'FetchAnswerCommentsController' to reflect changes made in the 'FetchAnswerCommentsUseCase' for listing comments of an answer, and using the 'CommentWithAuthorPresenter' instead of the 'CommentPresenter'. Updating controller tests to reflect these changes

--- src/infra/http/controllers/fetch-answer-comments.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-answer-comments.controller.e2e-spec.ts
@@ -42,7 +42,9 @@ describe('Fetch Answer Comments (E2E)', () => {
   })
 
   test('[GET] /answers/:answerId/comments', async () => {
-    const user = await studentFactory.makePrismaStudent()
+    const user = await studentFactory.makePrismaStudent({
+      name: 'John Doe'
+    })
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
@@ -77,12 +79,14 @@ describe('Fetch Answer Comments (E2E)', () => {
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
-      answerComments: expect.arrayContaining([
+      comments: expect.arrayContaining([
         expect.objectContaining({
-          content: 'Comment 01'
+          content: 'Comment 01',
+          authorName: 'John Doe'
         }),
         expect.objectContaining({
-          content: 'Comment 02'
+          content: 'Comment 02',
+          authorName: 'John Doe'
         })
       ])
     })
